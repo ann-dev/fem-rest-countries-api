2b76fce858b2e2cb26f3e075da65a9cc9029b9f9
Ann Herbik <45009615+ann-dev@users.noreply.github.com>
2021-02-13 23:26:47 +0100

    Header desktop, toggle icon done

--- site/src/components/shared/Header.js
+++ site/src/components/shared/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import MoonIcon from 'assets/moon-outline.svg';
 
 const Header = () => (
   <header className="z-10 relative shadow">
-    <div className="container mx-auto 2xl px-4 py-8 pb-7 flex flex-row flex-1 justify-between">
-      <h1 className="text-sm font-extrabold">Where in the world?</h1>
-      <button className="text-xs tracking-wide">Dark Mode</button>
+    <div className="container mx-auto 2xl px-4 lg:px-0.5 py-8 lg:py-6 pb-7 flex flex-row flex-1 justify-between">
+      <h1 className="text-sm lg:text-2xl font-extrabold">
+        Where in the world?
+      </h1>
+      <button className="text-xs lg:text-base font-semibold tracking-wide">
+        <img
+          src={MoonIcon}
+          className="inline-block w-4 lg:w-5 h-4 lg:h-5 -mt-1 mr-2"
+          alt=""
+          role="presentation"
+        />
+        <span>Dark Mode</span>
+      </button>
     </div>
   </header>
 );
