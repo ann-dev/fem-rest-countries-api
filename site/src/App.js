085ba53cfaa3fb1b69090d2a876e7adad7248df0
Ann Herbik <45009615+ann-dev@users.noreply.github.com>
2021-02-14 22:46:56 +0100

    Axios fetch request

--- site/src/App.js
+++ site/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from 'components/shared/Header';
 import Home from 'pages/Home';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Switch>
         <Route path="/" component={Home} />
       </Switch>
-    </>
+    </Router>
   );
 }
 
--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import DefaultLayout from 'pages/layouts/DefaultLayout';
 import Navigation from 'components/homepage/Navigation';
 import Card from 'components/common/Card';
-import Data from 'fakeData.json';
 
-const listCards = Data.map((item) => (
-  <Card
-    key={item.alpha2Code}
-    flagSrc={item.flag}
-    countryName={item.name}
-    population={item.population}
-    region={item.region}
-    capital={item.capital}
-  />
-));
+function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countries, setCountries] = useState([]);
 
-const Home = () => {
+  useEffect(() => {
+    setIsLoading(true);
+    const API_URL =
+      'https://restcountries.eu/rest/v2/all?fields=name;population;capital;flag;region;alpha2Code;';
+    axios
+      .get(API_URL)
+      .then((countries) => {
+        const allCountries = countries.data;
+        setIsLoading(false);
+        setCountries(allCountries);
+      })
+      .catch((err) => console.log('Error:', err));
+  }, []);
+
+  const listCards = countries.map((item) => (
+    <Card
+      key={item.alpha2Code}
+      flagSrc={item.flag}
+      countryName={item.name}
+      population={item.population}
+      region={item.region}
+      capital={item.capital}
+    />
+  ));
+
   return (
     <>
       <DefaultLayout>
         <Navigation />
         <div className="mt-8 md:-mt-2 grid gap-y-10 xl:gap-20 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {listCards}
+          {!isLoading ? listCards : <h4>Loading...</h4>}
         </div>
       </DefaultLayout>
     </>
   );
-};
+}
 
 export default Home;
